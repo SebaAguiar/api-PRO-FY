bf4d3fd0e09d964885d42846b1f0d64b18e61664
Aguiar David Sebastian <104685013+SebaAguiar@users.noreply.github.com>
2022-11-25 12:33:44 -0300

    Update admin.router.js

--- config/admin.router.js
+++ config/admin.router.js
@@ -1,15 +1,15 @@
-const { default: AdminBro } = require('admin-bro')
-const { buildRouter } = require('@admin-bro/express')
-const express = require('express')
+// const { default: AdminBro } = require('admin-bro')
+// const { buildRouter } = require('@admin-bro/express')
+// const express = require('express')
 
-/**
- * @param {AdminBro} admin
- * 
- * @return { Express.Router} router
- */
-const buildAdminRouter = (admin) => {
-  const router = buildRouter(admin)
-  return router
-}
+// /**
+//  * @param {AdminBro} admin
+//  * 
+//  * @return { Express.Router} router
+//  */
+// const buildAdminRouter = (admin) => {
+//   const router = buildRouter(admin)
+//   return router
+// }
 
-module.exports = buildAdminRouter
+// module.exports = buildAdminRouter
